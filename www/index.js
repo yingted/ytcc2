c8214519cd87929bea8b0e1c8a3c87a2f1e3c744
Ted Ying <yingted@gmail.com>
2020-10-20 06:09:46 -0400

    Add lit-html.

--- www/index.js
+++ www/index.js
@@ -1,12 +1,42 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const express = require('express');
+const { html, renderToStream } = require('@popeindustries/lit-html-server');
+
 const app = express();
+app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  renderToStream(html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <title>Captions editor</title>
+      </head>
+      <body>
+        <script src="/main.bundle.js"></script>
+        <noscript>You need JavaScript to view this page.</noscript>
+      </body>
+    </html>
+  `).pipe(res);
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
