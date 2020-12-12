0068e098c10354ae743acd69c5968a9d734f02ed
Ted Ying <yingted@gmail.com>
2020-12-12 07:26:35 -0500

    Set fixed viewport width.

--- www/index.js
+++ www/index.js
@@ -31,7 +31,7 @@ app.get('/watch', (req, res) => {
       <head>
         <meta charset="UTF-8" />
         <title>Captions viewer</title>
-        <meta name="viewport" content="width=640">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="/dialog-polyfill/dialog-polyfill.css" />
       </head>
       <body style="margin: 0;">
