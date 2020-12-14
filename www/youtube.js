4f1c0e80731e1f722f6b071f715c4b6291fae57e
Ted Ying <yingted@gmail.com>
2020-12-14 05:42:54 -0500

    Fix error with no script tags in the DOM.

--- www/youtube.js
+++ www/youtube.js
@@ -31,8 +31,7 @@ function waitForYouTubeIframeAPI() {
   var tag = document.createElement('script');
 
   tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  document.head.appendChild(tag);
 
   return waitForYouTubeIframeAPI();
 }
