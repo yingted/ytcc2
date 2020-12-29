f55f62d781ebcefdcaf75ceca54d8f0d6bad3e2e
Ted Ying <yingted@gmail.com>
2020-12-29 15:51:05 -0500

    Polyfill BigInt.

--- www/base52.js
+++ www/base52.js
@@ -3,14 +3,16 @@
  * Not constant-time.
  */
 
+const JSBI = require('jsbi');
+
 let alphabet = (function() {
-  let chars = ''; 
+  let chars = '';
   for (let i = 0; i < 256; ++i) {
     let c = String.fromCharCode(i);
     // Remove vowels to avoid offensive strings:
     if (/[0-9a-z]/i.test(c) && !/[aeiou]/i.test(c)) {
       chars += c;
-    }   
+    }
   }
   return chars;
 })();
@@ -28,17 +30,17 @@ function encode(length, bytes) {
   if (bytes.length !== length) {
     throw new RangeError('invalid length');
   }
-  const base = BigInt(alphabet.length);
+  const base = JSBI.BigInt(alphabet.length);
 
-  let x = 0n; 
+  let x = JSBI.BigInt(0);
   for (let c of bytes) {
-    x = x * 256n + BigInt(c);
+    x = JSBI.add(JSBI.multiply(x, JSBI.BigInt(256)), JSBI.BigInt(c));
   }
 
-  let id = []; 
-  while (x !== 0n) {
-    id.push(alphabet[Number(x % base)]);
-    x = x / base;
+  let id = [];
+  while (JSBI.notEqual(x, JSBI.BigInt(0))) {
+    id.push(alphabet[JSBI.toNumber(JSBI.remainder(x, base))]);
+    x = JSBI.divide(x, base);
   }
   return id.reverse().join('');
 }
@@ -52,19 +54,19 @@ function decode(length, base52) {
   if (typeof base52 !== 'string') {
     throw new TypeError('expected string');
   }
-  const base = BigInt(alphabet.length);
+  const base = JSBI.BigInt(alphabet.length);
 
-  let x = 0n; 
+  let x = JSBI.BigInt(0);
   for (let c of base52) {
     let i = alphabet.indexOf(c);
     if (i === -1) throw new RangeError('invalid character');
-    x = x * base + BigInt(i);
+    x = JSBI.add(JSBI.multiply(x, base), JSBI.BigInt(i));
   }
 
-  let id = []; 
-  while (x !== 0n) {
-    id.push(Number(x % 256n));
-    x = x / 256n;
+  let id = [];
+  while (JSBI.notEqual(x, JSBI.BigInt(0))) {
+    id.push(JSBI.toNumber(JSBI.remainder(x, JSBI.BigInt(256))));
+    x = JSBI.divide(x, JSBI.BigInt(256));
   }
   while (id.length < length) {
     id.push(0);
