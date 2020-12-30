46be5c8b77e329fbbefd0410a7e3adc22a4e4fd3
Ted Ying <yingted@gmail.com>
2020-12-29 21:02:08 -0500

    Fix download links not being updated.

--- www/main.js
+++ www/main.js
@@ -1435,9 +1435,9 @@ class FileMenu {
       let srtBlob = new ObjectUrl();
       let srv3Url;
       let srtUrl;
-      let key = editor._rawCaptions === empty ? FileMenu.EMPTY_CAPTIONS : editor._rawCaptions;
-      let updateSrv3 = () => srv3Url = srv3Blob.create(key, () => new Blob([editor.getSrv3Captions()]));
-      let updateSrt = () => srtUrl = srtBlob.create(key, () => new Blob([editor.getSrtCaptions()]));
+      let key = () => editor._rawCaptions === empty ? FileMenu.EMPTY_CAPTIONS : editor._rawCaptions;
+      let updateSrv3 = () => srv3Url = srv3Blob.create(key(), () => new Blob([editor.getSrv3Captions()]));
+      let updateSrt = () => srtUrl = srtBlob.create(key(), () => new Blob([editor.getSrtCaptions()]));
       updateSrv3();
       updateSrt();
       while (!this._isAborted) {
